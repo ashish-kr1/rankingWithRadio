6b5c54e7fd25c0c109f82225af2c2ac05bfddfed
ashishKr2 <ashish.23213@lpu.co.in>
2019-06-22 11:24:46 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  rank_no: number = 5;
-  rankArray: any;
-  noOfOptions: number = 2;
-  x: any;
   options = [{
     title: "option",
     rank: 0,
@@ -20,24 +16,18 @@ export class AppComponent {
   constructor() {
 
   }
-  ngOnInit(): void {
-    this.rankArray = Array(this.rank_no).fill().map((x, i) => i);
-    console.log(this.rankArray)
-  }
+  ngOnInit(): void { }
   addOption() {
     this.options.push({
       title: "option",
       rank: 0
     })
-
-    console.log(this.options);
   }
   deleteOption(i) {
     this.options.splice(i, 1);
   }
-  check(rank, event) {
-    console.log(event)
-    Array.from(document.getElementsByClassName(rank)).map(x=>x['checked']=false);
+  check(column_index, event) {
+    Array.from(document.getElementsByClassName(column_index)).map(x=>x['checked']=false);
     event.target.checked=true;
   }
 }
