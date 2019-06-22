174397bccaab6727a95d5f33c6e5548522687684
ashishKr2 <ashish.23213@lpu.co.in>
2019-06-22 14:29:34 +0530

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,11 +23,24 @@ export class AppComponent {
       rank: 0
     })
   }
-  deleteOption(i) {
-    this.options.splice(i, 1);
+  deleteOption(row_index) {
+    // const deletedRank=this.options[row_index].rank;
+    this.options.splice(row_index, 1);
+    // for(let i=0;i<this.options.length;i++){
+    //   if(this.options[i].rank>deletedRank){
+    //     console.log(this.options[i].rank,deletedRank)
+    //     this.options[i].rank=this.options[i].rank-1;
+    //   }
+    // }    
+   
   }
-  check(column_index, event) {
+  check(row_index,column_index, event) {
     Array.from(document.getElementsByClassName(column_index)).map(x=>x['checked']=false);
     event.target.checked=true;
+    this.options.map((x)=>{if(x.rank==column_index+1){
+      x.rank=0;
+    }})
+    this.options[row_index].rank=column_index+1;
+    console.log("rank",this.options)
   }
 }
