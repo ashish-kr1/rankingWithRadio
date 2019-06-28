f21eae5e439795354d03388607393dcec5a02061
ashishKr2 <ashish.23213@lpu.co.in>
2019-06-28 18:52:38 +0530

    with javascript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild, QueryList, ContentChildren } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,41 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  // options = [{
+  //   title: "option",
+  //   rank: 0,
+  //   scale: [0, 1]
+  // }, {
+  //   title: "option",
+  //   rank: 0,
+  //   scale: [0, 1]
+  // }]
   options = [{
     title: "option",
     rank: 0,
+    scale: [{ "selected": false, "index": 0 }, { "selected": false, "index": 1 }]
   }, {
     title: "option",
-    rank: 0
+    rank: 0,
+    scale: [{ "selected": false, "index": 0 }, { "selected": false, "index": 1 }]
   }]
-  constructor() {
-
+  constructor(private renderer: Renderer2, private el: ElementRef) {
   }
+
   ngOnInit(): void { }
   addOption() {
+    let scale = this.options[0].scale.map(object => ({ ...object }));
     this.options.push({
       title: "option",
-      rank: 0
-    })
+      rank: 0,
+      scale: scale
+    });
+    this.options.map(x => x.scale.push({ "selected": false, "index": this.options.length - 1 }));
+
+    // console.log("scale", this.options)
   }
   deleteOption(row_index) {
-    // const deletedRank=this.options[row_index].rank;
+    this.options.map(x => x.scale.pop());
     this.options.splice(row_index, 1);
-    // for(let i=0;i<this.options.length;i++){
-    //   if(this.options[i].rank>deletedRank){
-    //     console.log(this.options[i].rank,deletedRank)
-    //     this.options[i].rank=this.options[i].rank-1;
-    //   }
-    // }    
-   
   }
-  check(row_index,column_index, event) {
-    Array.from(document.getElementsByClassName(column_index)).map(x=>x['checked']=false);
-    event.target.checked=true;
-    this.options.map((x)=>{if(x.rank==column_index+1){
-      x.rank=0;
-    }})
-    this.options[row_index].rank=column_index+1;
-    console.log("rank",this.options)
+  check(row_index, option, column_index) {
+    //   Array.from(document.getElementsByClassName(column_index)).map(x=>x['checked']=false); 
+    //   event.target.checked=true;
+    this.options.map(x => x.scale[column_index].selected = false);
+    option.selected = true;
+    this.options.map((x) => {
+      if (x.rank == column_index + 1) {
+        x.rank = 0;
+      }
+    })
+    this.options[row_index].rank = column_index + 1;
+    console.log(this.options)
   }
 }
